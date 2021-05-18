443a6ae37173deb2a001fc3999b712209ffd7cdf
Debarghya Sarkar <57951516+dsarkar-dev@users.noreply.github.com>
2021-05-18 18:00:33 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,47 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+
+
+@st.cache
+
+def load_data():
+    df = pd.read_csv("data/all_stocks_5yr.csv",index_col="date")
+
+    numeric_df = df.select_dtypes(['float', 'int'])
+    numeric_cols = numeric_df.columns
+
+    text_df = df.select_dtypes(['object'])
+    text_cols= text_df.columns
+
+    stock_cols =df['Name']
+    unique_stocks = stock_cols.unique()
+    return df, numeric_cols, text_cols, unique_stocks
+
+
+df, numeric_cols, text_cols, unique_stocks = load_data()
+
+
+st.title("Stock Dashboard")
+
+check_box = st.sidebar.checkbox(label="Display dataset")
+
+if check_box:
+    st.write(df)
+
+st.sidebar.title("Settings")
+st.sidebar.subheader("Timeseries Setting")
+feature_selection = st.sidebar.multiselect(label = "Features to Plot", options =numeric_cols )
+
+stock_dropdown = st.sidebar.selectbox(label="Stock ticker", options = unique_stocks)
+
+
+print(feature_selection)
+df = df[df['Name'] == stock_dropdown]
+df_features =df[feature_selection]
+
+plotly_figure = px.line(data_frame=df_features, x=df_features.index, y=feature_selection, title=(str(stock_dropdown) + ' ' +'Timeline'))
+
+
+
+st.plotly_chart(plotly_figure)
